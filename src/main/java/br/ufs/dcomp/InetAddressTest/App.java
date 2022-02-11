406e503d47ee28b8354b75a26ece98afe6e06d99
Ubuntu <ubuntu@ip-172-31-40-199.ec2.internal>
2022-02-11 23:39:08 +0000

    Introdução ao InetAddress e ao Maven.

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -2,22 +2,22 @@ package br.ufs.dcomp.InetAddressTest;
 
 import java.net.*;
 
-/**
+/*
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        try{
+ 
+public class App {
+    public static void main( String[] args ) {
+        try {
             
-            String name = "www.uol.com.br";
+            // String name = "www.uol.com.br";
+            String name = "www.ufs.br";
             InetAddress address = InetAddress.getByName(name); 
-            System.out.println( "Name:      "+ name);
-            System.out.println( "Address:   "+ address.getHostAddress());
-
-        } catch (UnknownHostException e){
+            System.out.println( "Name:      " + name);
+            System.out.println( "Address:   " + address.getHostAddress());
+        } 
+        catch (UnknownHostException e) {
             System.out.println(e.getMessage());
         }                
     }
